b4f988a563c1e477c23726d3cba013a65203a3c8
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-01-23 08:49:29 -0300

    Ajustes

--- lib/chat_page/chat_page_widget.dart
+++ lib/chat_page/chat_page_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import '../flutter_flow/flutter_flow_icon_button.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
