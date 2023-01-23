6201a5f124ee9053c4716ac964d6cc5ab7f7a93a
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-01-23 10:35:24 -0300

    Ajustes

--- lib/all_chats_page/all_chats_page_widget.dart
+++ lib/all_chats_page/all_chats_page_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 
 import '../auth/auth_util.dart';
+import '../create_account/create_account_widget.dart';
 import '../flutter_flow/flutter_flow_animations.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
@@ -11,6 +12,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../main.dart';
 
 class LoginWidget extends StatefulWidget {
   const LoginWidget({Key? key}) : super(key: key);
